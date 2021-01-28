9afc74c2dd577892bd526189ccd4c6c92d8fd374
Aziko050050 <48541880+yakuza123456789@users.noreply.github.com>
2021-01-28 20:04:35 +0600

    Created Activity

--- app/src/main/java/com/tilek/test2/AzaActivity2.kt
+++ app/src/main/java/com/tilek/test2/AzaActivity2.kt
@@ -3,12 +3,9 @@ package com.tilek.test2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AzaActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        
-
+        setContentView(R.layout.activity_aza2)
     }
 }
